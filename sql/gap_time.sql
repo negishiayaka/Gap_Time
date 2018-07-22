f5c8d2985b311803576d9814ab8bf6559b7190f9
XNCOAISN <end.pgm@gmail.com>
2018-07-22 22:05:19 +0900

    SQLファイル更新 usersにログインフラグを追加

--- sql/gap_time.sql
+++ sql/gap_time.sql
@@ -13,8 +13,9 @@ create table users(
 	password varchar(16) not null comment "パスワード",
 	name varchar(100) not null comment "表示名",
 	introductions varchar(500) not null default "" comment "紹介文",
+	logined tinyint not null default 0 comment "ログインフラグ",
 	created_at datetime not null comment "作成日",
 	updated_at datetime not null comment "更新日"
 );
 
-insert into users values(1, "guest", "guest", "guest", "introductions", now(), now());
+insert into users values(1, "guest", "guest", "guest", "introductions", 0, now(), now());
