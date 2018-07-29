87b47d837473384f7b04102439717190d7d72f5e
XNCOAISN <end.pgm@gmail.com>
2018-07-29 16:22:52 +0900

    文字数制限判定メソッド修正

--- src/com/internousdev/gap_time/util/InputChecker.java
+++ src/com/internousdev/gap_time/util/InputChecker.java
@@ -3,6 +3,6 @@ package com.internousdev.gap_time.util;
 public class InputChecker {
 
 	public static boolean length(String value, int min, int max){
-		return value.length() < min || value.length() > max;
+		return min <= value.length() && value.length() <= max;
 	}
 }
